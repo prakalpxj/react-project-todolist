35cad5bcea00dd3d080a96404c9162560ffe38a7
prakalpxj <prakalp.jain.05@gmail.com>
2020-07-15 17:06:32 +0530

    github check (second)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import uuid from "uuid"
 import "bootstrap/dist/css/bootstrap.min.css"
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/TodoInput"
+//github-auto-commit-check
 function App() {
   return (
     <div>
