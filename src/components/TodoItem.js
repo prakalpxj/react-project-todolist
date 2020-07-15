c8ef7b5fd385000f47eeab1fc788792a6fae55ae
prakalpxj <prakalp.jain.05@gmail.com>
2020-07-15 16:52:09 +0530

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <h1>Hello from TodoItem</h1>
+        )
+    }
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Item from "./TodoItem"
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <h1>Hello from Todolist</h1>
+        )
+    }
+}
